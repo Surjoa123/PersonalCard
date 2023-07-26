764b1c686964366868f274ed33df1e2965f66e6c
Surjoa123 <124343205+Surjoa123@users.noreply.github.com>
2023-07-26 17:40:33 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,26 @@
+var typed = new Typed('.typer', {
+    strings: ['Developer.', 'Coder.','Designer.','Music Lover.','Follower of Krishna.'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+   });
+
+// // Get references to the button and contact form
+// const openContactFormBtn = document.getElementById('openContactFormBtn');
+// const contactForm = document.getElementById('contactForm');
+
+// // Show the contact form when the button is clicked
+// openContactFormBtn.addEventListener('click', () => {
+//   contactForm.style.display = 'block';
+// });
+
+// // Handle form submission
+// const contactFormContent = document.getElementById('contactFormContent');
+// contactFormContent.addEventListener('submit', (e) => {
+//   e.preventDefault(); // Prevent the form from submitting
+
+//   // Perform form data validation here if needed
+
+//   // Hide the contact form after submission
+//   contactForm.style.display = 'none';
+// });
